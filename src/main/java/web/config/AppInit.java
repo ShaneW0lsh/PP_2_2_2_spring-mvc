3750a809ceef2b31a988aa92d24537c42b2214b7
Shane <shanebetterfather@gmail.com>
2024-05-29 12:36:02 +0300

    Complete task

--- src/main/java/web/config/AppInit.java
+++ src/main/java/web/config/AppInit.java
@@ -14,8 +14,8 @@ public class AppInit extends AbstractAnnotationConfigDispatcherServletInitialize
     // Добавление конфигурации, в которой инициализируем ViewResolver, для корректного отображения jsp.
     @Override
     protected Class<?>[] getServletConfigClasses() {
-        return new Class<?>[]{
-                WebConfig.class
+        return new Class<?>[] {
+            WebConfig.class
         };
     }
 
@@ -23,7 +23,7 @@ public class AppInit extends AbstractAnnotationConfigDispatcherServletInitialize
     /* Данный метод указывает url, на котором будет базироваться приложение */
     @Override
     protected String[] getServletMappings() {
-        return new String[]{"/"};
+        return new String[] {"/"};
     }
 
 }
